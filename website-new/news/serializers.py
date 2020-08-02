b001a21e56484fa81cdcdcf7b3e0a53d5a5974c9
kevin952 <dsouza.kevin952@gmail.com>
2020-08-03 00:52:42 +0530

    new website update

---
+++ website-new/news/serializers.py
@@ -0,0 +1,7 @@
+# from rest_framework import serializers
+# from .models import relevance
+
+# class relevanceSerializers(serializers.Serializer):
+#     class meta:
+#         model = relevance
+#         fields = '__all__'
