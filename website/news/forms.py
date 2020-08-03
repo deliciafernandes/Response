6d9e5a6825291e1351db5cce68f45b7c1aa520ad
kevin952 <dsouza.kevin952@gmail.com>
2020-08-03 11:47:23 +0530

    plugin update

--- website/news/forms.py
+++ website/news/forms.py
@@ -2,19 +2,7 @@ from django import forms
 from django.core.validators import RegexValidator
 import http.client
 
-def sendPhoneOTP(contact):
-	conn = http.client.HTTPConnection("2factor.in")
-	phone = contact[3:]
-	payload = ""
-	headers = { 'content-type': "application/x-www-form-urlencoded" }
-	conn.request("GET", "/API/V1/293832-67745-11e5-88de-5600000c6b13/SMS/phone/AUTOGEN", payload, headers)
-	res = conn.getresponse()
-	data = res.read()
-	print(data.decode("utf-8"))
-	return data
 
-def verifyOTP(contact):
-	pass
 
 
 
@@ -27,10 +15,5 @@ class SubscribeForm(forms.Form):
     contact = forms.CharField(validators=[phone_regex], max_length=17,required = True,  widget= forms.TextInput(attrs ={'class':"input"})) # validators should be a list
 
 
-    def sendOTP(self):
-    	data = self.contact
-    	if not sendPhoneOTP(contact):
-    		raise forms.ValidationError('OTP verification not done')	
 
-    	return sendPhoneOTP(contact)
     
