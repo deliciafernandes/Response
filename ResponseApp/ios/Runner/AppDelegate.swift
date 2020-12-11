1ec73499add780fe8a5ce4c9b0148d83ec319698
Delicia Fernandes <fernandesdelicia07@gmail.com>
2020-12-11 21:49:13 +0530

    Response App v1.0.1

---
+++ ResponseApp/ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+
+    GMSServices.provideAPIKey("PUT_YOUR_API_KEY_HERE")
+    GeneratedPluginRegistrant.register(with: self)
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
